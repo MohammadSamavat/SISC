1451df7fd859ad19be8cf6d4eba6cd7f67315fd9
Mohammad Samavat <49333269+MohammadSamavat@users.noreply.github.com>
2024-06-05 15:08:05 -0700

    Create Main.r

---
+++ main.r
@@ -0,0 +1,4 @@
+# The R code for Algorithm 1 and 2 published in Samavat et al, Neural Computation 2024. 
+
+# Acknowledgments
+# We thank Adel Aghajan and Wenxin Zhou for helpful discussion regarding information theory and bootstrapping analysis. 
